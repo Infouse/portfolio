e916b1ef27af04b54b16303eeed0e994120c5048
infouse <hongyeong575@naver.com>
2024-11-15 17:10:03 +0900

    lhy

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../style/Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <h2 className='portfolioTitle'>Contact Me</h2>
+    <div className='contactBox'>
+      <div className='contactTextBox'>
+        <p>Name : 이 홍 영</p>
+        <p>Email : hongyeong575@naver.com</p>
+      </div>
+      <div className='contactTextBox'>
+        <p>CellPhone : 010-6660-1578</p>
+        <p>residence : Incheon</p>
+      </div>  
+    </div>
+
+    <h2 className='endText'>끝까지 봐주셔서 감사합니다.</h2>
+    </>
   )
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/Header.scss'
 const Header = () => {
+  const [scroll, setScroll] = useState(false);
 
-  const aboutScroll = () => {
-    window.scrollTo({
-      top : 525,
-      behavior : 'smooth'
-    });
+  const handleScroll = ()=>{
+    if (window.scrollY > 50) {
+      setScroll(true);
+    }
+    else{
+      setScroll(false);
+    }
   }
+  
+    const topScroll = () =>{
+      window.scrollTo({
+        top : 0,
+        behavior : 'smooth'
+      });
+    }
     const projectScroll = () => {
       window.scrollTo({
-        top : 1156,
+        top : 1100,
         behavior : 'smooth'
       });
     }
@@ -29,9 +39,8 @@ const Header = () => {
 
   return (
     <div className='headerBox'>
-      <p className='headerText'>Portfolio</p>
+      <p onClick={topScroll} className='headerText'>Portfolio</p>
       <div className='headerFlexBox'>
-        <p onClick={aboutScroll} className='headerText'>About me</p>
         <p onClick={projectScroll} className='headerText'>Team Project</p>
         <p onClick={persnoalScroll} className='headerText'>Personal Project</p>
         <p onClick={techScroll} className='headerText'>Tech Stack</p>
